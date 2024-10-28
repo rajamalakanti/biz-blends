912649fad11706e7becee51e03b36c926806cfaa
rajkamalamalakanti <rajkamal.amalakanti@gmail.com>
2024-10-28 17:46:10 -0500

    hi

--- app/book/page.tsx
+++ app/book/page.tsx
@@ -4,11 +4,8 @@ import {
     Card,
     CardContent,
     CardDescription,
-    CardFooter,
     CardTitle,
 } from "@/components/ui/card";
-import { Button } from '@/components/ui/button';
-import Link from 'next/link';
 
 const CombinedPage = () => {
   return (
